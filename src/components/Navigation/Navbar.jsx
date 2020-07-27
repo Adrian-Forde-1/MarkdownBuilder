e194b990eb36c84e098f904dd716a26b3b3c07d0
Adrian <AdrianForde02@gmail.com>
2020-07-27 17:45:21 -0300

    Made the navbar responsive but still need to fix an overflow error

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -7,10 +7,22 @@ import '../../styles/navbar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  window.addEventListener('scroll', (e) => {
+    if (window.pageYOffset <= 0) {
+      document.querySelector('#navbar').style.background = '#9E2A2B';
+    } else {
+      document.querySelector('#navbar').style.background = '#1e1e1e';
+    }
+  });
   return (
-    <div className="navbar bg-gray-800 lg:px-40 px-16 lg:py-0 py-2 flex flex-wrap items-center ">
-      <div className="flex-1 flex justify-between items-center text-pink-800 font-bold">
-        <Link to="/">Markdown</Link>
+    <div
+      className="navbar w-full ease-out bg-theme_red-100 lg:px-40 px-12 lg:py-0 py-2 flex flex-wrap items-center fixed inset-x-0 top-0"
+      style={{ transform: 'translateY(-100px)' }}
+      id="navbar"
+      style={{ zIndex: '100' }}
+    >
+      <div className="flex-1 flex justify-between items-center text-white">
+        <Link to="/">Markdown Creator</Link>
       </div>
 
       <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
@@ -18,12 +30,13 @@ const Navbar = () => {
           width="1em"
           height="1em"
           viewBox="0 0 16 16"
-          className="bi bi-list text-pink-800"
+          className="bi bi-list text-white"
           fill="currentColor"
           xmlns="http://www.w3.org/2000/svg"
+          style={{ fontSize: '20px' }}
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
           />
         </svg>
@@ -32,20 +45,14 @@ const Navbar = () => {
 
       <div className="hidden lg:flex lg:item-center lg:w-auto w-full" id="menu">
         <nav>
-          <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+          <ul className="lg:flex items-center justify-between text-base pt-4 lg:pt-0">
             <li>
-              <Link
-                to="#"
-                className="lg:p-2 py-2 px-0 block border-b-2 text-white border-transparent hover:border-pink-800"
-              >
+              <Link to="#" className="nav-item" style={{ fontWeight: '300' }}>
                 Home
               </Link>
             </li>
             <li>
-              <Link
-                to="#"
-                className="lg:p-2 py-2 px-0 block border-b-2 text-white border-transparent hover:border-pink-800"
-              >
+              <Link to="#" className="nav-item" style={{ fontWeight: '300' }}>
                 About
               </Link>
             </li>
