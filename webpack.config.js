02e19a15b0f9d289abd0407c185316941f911257
Adrian <AdrianForde02@gmail.com>
2020-07-29 17:54:28 -0300

    Fixed the issue where when I reloaded a page other than the homepage on router=/, I would get a 404 error

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/assets/',
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   module: {
     rules: [
