ae7ab437339e811b70da4b25ab632e52ea2472fa
Adrian <AdrianForde02@gmail.com>
2020-08-01 23:43:42 -0300

    Updated the root reducer with the new view reducer

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 
 //Reducers
 import builderReducer from './builderReducer';
+import viewReducer from './viewReducer';
 
 const rootReducer = combineReducers({
   builder: builderReducer,
+  views: viewReducer,
 });
 
 export default rootReducer;
