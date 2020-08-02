2947acd58b977122c603a88822ffe7c6fbc15aeb
Adrian <AdrianForde02@gmail.com>
2020-08-01 22:46:06 -0300

    Added a view reducer for managing views

---
+++ src/redux/reducers/viewReducer.js
@@ -0,0 +1,20 @@
+import { BUILDER_VIEW, PREVIEW_VIEW, RAW_VIEW } from '../actions/types';
+
+const initialState = {
+  currentView: '',
+};
+
+const viewReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUILDER_VIEW:
+      return { ...state, currentView: 'builder' };
+    case PREVIEW_VIEW:
+      return { ...state, currentView: 'preview' };
+    case RAW_VIEW:
+      return { ...state, currentView: 'raw' };
+    default:
+      return state;
+  }
+};
+
+export default viewReducer;
